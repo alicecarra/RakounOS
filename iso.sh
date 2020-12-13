892d4006a84e01e903795c3d15f18461f3d71e7e
Vinícius Carra <vinicius.carra@gmail.com>
2020-12-13 18:51:41 -0300

    Name bug fixed

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/rakounos.kernel isodir/boot/rakounos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "rakounos" {
+	multiboot /boot/rakounos.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o rakounos.iso isodir
