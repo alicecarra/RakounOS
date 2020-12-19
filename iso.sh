bc13a4cfb69839b6f1f1f6cfd4eaa72b5be3f721
Vinícius Carra <vinicius.carra@gmail.com>
2020-12-18 23:51:04 -0300

    Fixed OS name in bootloader [rakounos -> RakounOS]

--- iso.sh
+++ iso.sh
@@ -8,8 +8,12 @@ mkdir -p isodir/boot/grub
 
 cp sysroot/boot/rakounos.kernel isodir/boot/rakounos.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "rakounos" {
+menuentry "RakounOS" {
+<<<<<<< HEAD
+	multiboot /boot/RakounOS.kernel
+=======
 	multiboot /boot/rakounos.kernel
+>>>>>>> aa30ab26f787dd11a4f5960513c88127d2bdf842
 }
 EOF
 grub-mkrescue -o rakounos.iso isodir
